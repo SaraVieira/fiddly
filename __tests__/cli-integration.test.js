1f3dcf789e6946978b5d3a378a1dab98f72661ba
Sara Vieira <hey@iamsaravieira.com>
2018-12-24 17:12:43 +0000

    delete test

--- __tests__/cli-integration.test.js
+++ __tests__/cli-integration.test.js
@@ -7,16 +7,6 @@ const success = `Generated your static files at public/`
 const cli = async cmd =>
   system.run('node ' + resolve(src, 'bin', 'fiddly') + ` ${cmd}`)
 
-test('outputs version', async () => {
-  const output = await cli('--version')
-  expect(output).toContain('0.0.1')
-})
-
-test('outputs help', async () => {
-  const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
-
 test('generates html', async () => {
   const output = await cli()
 
